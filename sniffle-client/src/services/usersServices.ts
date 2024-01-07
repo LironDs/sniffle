bc72fdc2cf6b8d10b40b454ec8cddbabdc7481fd
LironDs <david.liron@gmail.com>
2024-01-07 10:58:14 +0200

    update before deployment

--- sniffle-client/src/services/usersServices.ts
+++ sniffle-client/src/services/usersServices.ts
@@ -2,22 +2,22 @@ import axios from "axios";
 import User from "../interfaces/User";
 import jwt_decode from "jwt-decode";
 
-let api: string = `${process.env.REACT_APP_API}/users`;
+let api: string = `${process.env.REACT_APP_API}`;
 
 type userLogin = Pick<User, "email" | "password">;
 
 export function checkUser(userToCheck: userLogin) {
-  return axios.post(`http://localhost:10000/api/login`, userToCheck);
+  return axios.post(`${api}/login`, userToCheck);
 }
 
 ////no need for Authorization
 
 export function addUser(newUser: User) {
-  return axios.post(`http://localhost:10000/api/register`, newUser);
+  return axios.post(`${api}/register`, newUser);
 }
 
 export function updateUser(_id: string, updatedUser: User) {
-  return axios.put(`${api}/${_id}`, updatedUser, {
+  return axios.put(`${api}/users/${_id}`, updatedUser, {
     headers: {
       Authorization: JSON.parse(sessionStorage.getItem("token") as string).token,
     },
@@ -25,7 +25,7 @@ export function updateUser(_id: string, updatedUser: User) {
 }
 
 export function getUserById(_id: string) {
-  return axios.get(`${api}/${_id}`, {
+  return axios.get(`${api}/users/${_id}`, {
     headers: {
       Authorization: JSON.parse(sessionStorage.getItem("token") as string).token,
     },
@@ -33,7 +33,7 @@ export function getUserById(_id: string) {
 }
 
 export function getUsers() {
-  return axios.get(api, {
+  return axios.get(`${api}/users`, {
     headers: {
       Authorization: JSON.parse(sessionStorage.getItem("token") as string).token,
     },
@@ -41,7 +41,7 @@ export function getUsers() {
 }
 
 export function deleteUser(_id: string) {
-  return axios.delete(`${api}/${_id}`, {
+  return axios.delete(`${api}/users/${_id}`, {
     headers: {
       Authorization: JSON.parse(sessionStorage.getItem("token") as string).token,
     },
